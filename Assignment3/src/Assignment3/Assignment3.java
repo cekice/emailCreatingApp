fa208d81bd47cb76c837fd62250fd515c111ba74
Edin Cekic <cekic.edin@hotmail.com>
2018-07-20 15:34:23 +0200

    JavaBill

---
+++ Assignment3/src/Assignment3/Assignment3.java
@@ -0,0 +1,14 @@
+package Assignment3;
+
+public class Assignment3 {
+
+    public static void main(String[] args) {
+        
+        Chocolate chocolate = new Chocolate("Milka sa lješnjacima", 12345678, 3.30, 200);
+        System.out.println(chocolate);
+        
+        Wine wine = new Wine("Vino", 87654321, 6.80, 0.750 );
+        System.out.println(wine);
+    }
+    
+}
